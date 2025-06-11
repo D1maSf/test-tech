7b6576f467dac943878a31a8b0f8f9c8ec7d4b99
d1masf <d1manzbandit@gmail.com>
2025-06-11 22:59:46 +0300

    added local state for progressBar

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/router.js';
-import store from './store/pieChart.js';
+import store from './store/pieChart.js'
 
 const app = createApp(App);
 app.use(router);
---
+++ src/views/home.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <router-link
+        to="/progressBarDemo"
+        class="btn btn--default"
+    >
+      <span>Progress Bar</span>
+    </router-link>
+    <router-link
+        to="/pieChart"
+        class="btn btn--default"
+    >
+      <span>Pie Chart</span>
+    </router-link>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
